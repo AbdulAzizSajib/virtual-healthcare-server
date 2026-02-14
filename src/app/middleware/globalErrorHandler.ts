62cf1a908d950d007f35ac1e5669e87f4e2d7125
Abdul Aziz Sajib <abdulazizsajib@gmail.com>
2026-02-14 15:55:08 +0600

    add multer and cloudinary image upload

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -7,10 +7,11 @@ import { handleZodError } from "../errorHelpers/handleZodError";
 import z from "zod";
 import { status } from "http-status";
 import AppError from "../errorHelpers/AppError";
+import { deleteFileFromCloudinary } from "../config/cloudinary.config";
 
 //  global error handler
 
-export const globalErrorHandler = (
+export const globalErrorHandler = async (
   err: any,
   req: Request,
   res: Response,
@@ -20,6 +21,17 @@ export const globalErrorHandler = (
   if (envVars.NODE_ENV === "development") {
     console.error("Error from Global Error Handler:", err);
   }
+
+  // cleanup uploaded files if any error occurs during file upload or processing
+  if (req.file) {
+    await deleteFileFromCloudinary(req.file.path);
+  }
+
+  if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+    const imageUrls = req.files.map((file) => file.path);
+    await Promise.all(imageUrls.map((url) => deleteFileFromCloudinary(url)));
+  }
+  // handle different types of errors and send appropriate response
 
   let errorSources: TErrorSources[] = [];
   let statusCode: number = status.INTERNAL_SERVER_ERROR;
--- src/app/module/specialty/specialty.router.ts
+++ src/app/module/specialty/specialty.router.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { specialtyController } from "./specialty.controller";
+import { multerUpload } from "../../config/multer.config";
 
 const specialtyRouter = Router();
 
-specialtyRouter.post("/", specialtyController.createSpecialty);
+specialtyRouter.post(
+  "/",
+  multerUpload.single("file"),
+  specialtyController.createSpecialty,
+);
 specialtyRouter.get("/", specialtyController.getAllSpecialties);
 specialtyRouter.delete("/:id", specialtyController.deleteSpecialty);
 
